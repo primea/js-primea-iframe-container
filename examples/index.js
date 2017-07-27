733e63401b316066a1641cce4c7c6ff06a9e1a04
wanderer <mjbecze@gmail.com>
2017-07-26 23:46:46 -0400

    radom fixes

--- examples/index.js
+++ examples/index.js
@@ -4,78 +4,29 @@ const IframeContainer = require('../')
 const Message = require('primea-message')
 const fs = require('fs')
 
-const content = fs.readFileSync(__dirname + '/content.bundle.js')
+const content = fs.readFileSync(__dirname + '/rootContainer.bundle.js').toString()
 
 const node = new IPFS({
   start: false
 })
 
 // wait untill the ipfs node is ready
-node.on('ready', () => {
+node.on('ready', async () => {
   // create a new hypervisor instance
   const hypervisor = new Hypervisor(node.dag)
   hypervisor.registerContainer('iframe', IframeContainer)
 
   // create a root instance of the example container
-  hypervisor.createInstance('iframe', new Message({data: content}))
+  const root = await hypervisor.createInstance('iframe', new Message({
+    data: content
+  }))
 
-  // // create two channels
-  // const [portRef1, portRef2] = root.ports.createChannel()
-  // const [portRef3, portRef4] = root.ports.createChannel()
-
-  // // create two instances of the example container. These containers wiil be
-  // // given channels to the parent container
-  // root.createInstance('example', root.createMessage({
-  //   ports: [portRef2]
-  // }))
-
-  // root.createInstance('example', root.createMessage({
-  //   ports: [portRef4]
-  // }))
-
-  // // bind the ports of the channels to the newly created containers. Binding
-  // // ports allows the root container tt receieve messages from the containers.
-  // // If no other container bound these ports then the corrisponding containers
-  // // would be garbage collected
-  // root.ports.bind('one', portRef1)
-  // root.ports.bind('two', portRef3)
-
-  // // create a new channel. Each channel has two corrisponding ports that
-  // // containers can communicate over
-  // const [chanRef1, chanRef2] = root.ports.createChannel()
-
-  // // send the newly created ports to each of the containers. Once both the
-  // // recieving containers bind the ports they will be able to communicate
-  // // directly with each other over them
-  // await root.send(portRef1, root.createMessage({
-  //   data: 'bindPort',
-  //   ports: [chanRef1]
-  // }))
-
-  // await root.send(portRef3, root.createMessage({
-  //   data: 'bindPort',
-  //   ports: [chanRef2]
-  // }))
-
-  // // after the recieving containers bind the ports in the messages the channel
-  // // topology will look like this. Where "[]" are the containers, "*" are the
-  // // ports that the container have and "(name)" is the port name.
-  // //
-  // //        root container
-  // //            [ ]
-  // //      (one) * * (two)
-  // //           /    \
-  // //          /      \
-  // //         /        \
-  // // (parent)*          * (parent)
-  // //       [ ]*--------*[ ]
-  // //     (channel)    (channel)
-
-  // // create a new state root. The state root is not created untill the
-  // // hypervisor has finished all of it's work
-  // const stateRoot = await hypervisor.createStateRoot()
-  // console.log(stateRoot)
-  // console.log('--------full state dump---------')
-  // await hypervisor.graph.tree(stateRoot, Infinity)
-  // console.log(JSON.stringify(stateRoot, null, 2))
+  window.state = hypervisor.state
+  console.log(hypervisor.state)
+  const stateRoot = await hypervisor.createStateRoot(0)
+  console.log('create state')
+  console.log(stateRoot)
+  console.log('--------full state dump---------')
+  await hypervisor.graph.tree(stateRoot, Infinity)
+  console.log(JSON.stringify(stateRoot, null, 2))
 })
--- index.js
+++ index.js
@@ -1,81 +1,104 @@
+const ReferenceMap = require('reference-map')
 const createIframe = require('create-iframe')
 const IframeStream = require('iframe-stream').IframeStream
 const dnode = require('dnode')
 
 module.exports = class IframeContainer {
-  // the constructor is given an instance of the kernel
-  // https://github.com/primea/js-primea-hypervisor/blob/master/docs/kernel.md
   constructor (kernel) {
     this.kernel = kernel
-    this._setUpIframe()
+    this._iframeInit = false
+    this._refs = new ReferenceMap()
   }
 
   _setUpIframe () {
-    const code = this.kernel.state.code
-    if (code) {
-      this.iframe = createIframe(this.kernel.state.code, {
-        container: document.body,
-        sandbox: ['allow-scripts', 'allow-same-origin']
-      }).iframe
-
-      this.iframeStream = IframeStream(this.iframe)
-      const d = dnode({
-        getPort: (name, cb) => {
-          cb(this.ports.get(name))
-        },
-        storePort: (name, port, cb) => {
-          this.ports.set(name, port)
-          cb()
-        },
-        deletePort: (name, cb) => {
-          this.ports.delete(name)
-          cb()
-        },
-        createInstance: async (message, cb) => {
-          await this.create(IframeContainer.name, message)
-          cb()
-        },
-        createMessage: (data, ports, cb) => {
-          const message = this.createMessage({
-            data: data,
-            ports: ports
-          })
-          cb(message)
-        },
-        createChannel: cb => {
-          cb(this.createChannel())
-        },
-        send: (port, message, cb) => {
-          this.send(port, message)
-          cb()
-        },
-        id: cb => {
-          cb(this.id)
-        }
-      })
-      this.iframeStream.pipe(d).pipe(this.iframeStream)
-
-      return Promise((resolve, reject) => {
-        d.on('remote', remote => {
-          this.remote = remote
-          resolve()
+    if (!this._iframeInit) {
+      const code = this.kernel.state.code
+      if (code) {
+        let res
+        const self = this
+        const promise = new Promise((resolve, reject) => {
+          res = resolve
         })
-      })
+
+        this.iframe = createIframe(code, {
+          container: document.body,
+          sandbox: ['allow-scripts', 'allow-same-origin']
+        }).iframe
+
+        this.iframeStream = IframeStream(this.iframe)
+
+        const d = dnode(function (remote) {
+          self.remote = remote
+          this.getPort = (name, cb) => {
+            const port = self.kernel.ports.get(name)
+            cb(self._refs.add(port))
+          }
+          this.bindPort = async (name, port, cb) => {
+            port = self._refs.get(port)
+            await self.kernel.ports.bind(name, port)
+            cb()
+          }
+          this.deletePort = (name, cb) => {
+            self.kernel.ports.delete(name)
+            cb()
+          }
+          this.createInstance = async (message, cb) => {
+            message = self._refs.get(message.msgRef)
+            await self.kernel.createInstance(IframeContainer.type, message)
+            cb()
+          }
+          this.createMessage = (data, ports, cb) => {
+            const message = self.kernel.createMessage({
+              data: data,
+              ports: ports.map(ref => self._refs.get(ref))
+            })
+
+            const json = Object.assign({}, message.toJSON())
+            json.msgRef = self._refs.add(message)
+            json.ports = message.ports.map(port => self._refs.add(port))
+
+            cb(json)
+          }
+          this.createChannel = cb => {
+            const ports = self.kernel.ports.createChannel()
+            const port1 = self._refs.add(ports[0])
+            const port2 = self._refs.add(ports[1])
+            cb([port1, port2])
+          }
+          this.send = async (port, message, cb) => {
+            port = self._refs.get(port)
+            message = self._refs.get(message.msgRef)
+            await self.kernel.send(port, message)
+            cb()
+          }
+          this.id = cb => {
+            cb(self.kernel.id)
+          }
+          self._iframeInit = true
+          d.on('remote', res)
+        })
+        d.pipe(this.iframeStream).pipe(d)
+        return promise
+      }
     }
   }
 
-  // this method runs once when the container is intailly created. It is given
-  // a message with a single port, which is a channel to its parent with the
-  // exception of the root container (the container that is intailial created)
-  initialize (message) {
+  async initialize (message) {
     this.kernel.state.code = message.data
-    this._setUpIframe()
-    this.run(message, 'initialize')
+    delete message._opts.data
+    return this.run(message, 'initialize')
   }
 
   // the function is called for each message that a container gets
   async run (message, method = 'main') {
-    this.remote[method](message)
+    await this._setUpIframe()
+    const json = Object.assign({}, message.toJSON())
+    json.msgRef = this._refs.add(message)
+    json.ports = message.ports.map(port => this._refs.add(port))
+
+    return new Promise((resolve, reject) => {
+      this.remote[method](json, resolve)
+    })
   }
 
   onIdle () {}
